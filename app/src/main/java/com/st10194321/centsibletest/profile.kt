b06d80fd4226a988ea231074d66b16021f8af33a
Ethan Pillay <ST10258615@vcconnect.edu.za>
2025-05-01 11:31:41 +0200

    Logout button

--- app/src/main/java/com/st10194321/centsibletest/profile.kt
+++ app/src/main/java/com/st10194321/centsibletest/profile.kt
@@ -66,5 +66,14 @@ class profile : AppCompatActivity() {
             finish()
         }
 
+        binding.cardLogout.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            val intent = Intent(this, signin::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+            finish()
+        }
+
+
     }
 }
